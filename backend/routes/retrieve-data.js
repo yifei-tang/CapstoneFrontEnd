4dc5e8fc4aee86135cb84520185763a0d0cf5bfb
yifei-tang <yifeit.tang@mail.utoronto.ca>
2021-02-20 12:31:10 -0700

    changes

--- backend/routes/retrieve-data.js
+++ backend/routes/retrieve-data.js
@@ -5,6 +5,7 @@ router.route('/:City/:Year/:Type').get((req,res)=>{
     const city = req.params.City;
     const year = req.params.Year;
     const type = req.params.Type;
+    console.log("get");
     Retrieve.findOne({City:city, Year:year,Type:type})
     .then(City=>res.json(City))
     .catch(err=> res.status(400).json('Error: '+err));  
